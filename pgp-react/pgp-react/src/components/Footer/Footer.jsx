4b0963caccad2d85404da121328666639d89013e
tobias <tobiasmessa06@gmail.com>
2026-02-16 17:08:35 -0300

    Proyecto Final - Messa

--- pgp-react/pgp-react/src/components/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="footer">
-      © {new Date().getFullYear()} Patagonia Go Play. Todos los derechos reservados.
-    </footer>
-  );
-};
-
-export default Footer;
